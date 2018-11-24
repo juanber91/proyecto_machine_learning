6008bf5e56cfa052f43c19b6418975be53c1985a
Juan Mpc <jbmpc@outlook.com>
2018-11-24 16:07:22 -0600

    codigo lectura airbnb

--- 01_explora_airbnb.R
+++ 01_explora_airbnb.R
@@ -3,26 +3,31 @@ library(data.table)
 library(magrittr)
 
 # Cargamos datos y limpiamos ----------------------------------------------
-listings_raw <- fread('datos/listings.csv', encoding = 'UTF-8') %>% 
+archivos <- list.files('datos/')
+
+listings_raw <- map(archivos, ~fread(paste0('datos/', .),  encoding = 'UTF-8')) %>% 
+  bind_rows
+
+listings_raw %<>% 
   select(-c(contains('url'), description, scrape_id, name, summary, space, neighborhood_overview, notes,
             transit, access, interaction, house_rules, host_name, host_location, host_about,
             host_neighbourhood, neighbourhood, neighbourhood_cleansed, city, state, 
-            market:country, amenities, host_verifications)) %>% 
+            market:country_code, amenities, host_verifications)) %>% 
   mutate(price = parse_number(price),
-         zipcode = as.numeric(zipcode))
-
-listings_raw %>% head
+         zipcode = as.numeric(zipcode)) %>% 
+  filter(country != 'Switzerland')
 
 listing_mod <- listings_raw %>% 
-  select(id, price, review_scores_rating, room_type, accommodates, bathrooms, 
+  select(id, country, price, review_scores_rating, room_type, accommodates, bathrooms, 
            bedrooms, neighbourhood_group_cleansed, zipcode) %>% 
   na.omit() %>% 
   mutate_if(is.character, funs(as.factor)) %>% 
-  filter(price > 0, price < 250)
+  filter(price > 0, price <= 250)
 
 listing_mod %>% str
 data.frame(colSums(is.na(listing_mod)))
 
+set.seed(815)
 listing_mod$price %>% quantile(seq(0, 1, 0.01))
 
 
@@ -31,8 +36,8 @@ entrena <- sample_frac(listing_mod, 0.75)
 prueba <- listing_mod %>% filter(!id %in% entrena$id) %>% select(-id)
 entrena %<>% select(-id)
 
-modelo <- lm(price ~ review_scores_rating + accommodates + bathrooms + 
-               bedrooms + neighbourhood_group_cleansed + room_type,
+modelo <- lm(price ~ country + review_scores_rating + accommodates + bathrooms + 
+               bedrooms + room_type,
              data = entrena)
 
 summary(modelo)
